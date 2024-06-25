9dac9a80ae92c3612a0fda2cbba95268c92061b3
bella <bellawritefashion@gmail.com>
2024-06-25 17:55:41 +0200

    Upadte chnages

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-alert("welcome to sinko pa tez designers, your custom ready to wear provider.")
-let 
+// alert("welcome to sinko pa tez designers, your custom ready to wear provider.")
+// let 
+
+const mobileMenu = document.querySelector('.mobile-menu')
+const mobileOpen = document.querySelector('.fa-bars')
+const mobileClose = document.querySelector('.fa-times')
+
+mobileOpen.addEventListener('click', function() {
+    console.log("wre")
+    mobileMenu.style.display = "block"
+    mobileOpen.style.display = "none"
+    mobileClose.style.display = "block"
+})
+
+mobileClose.addEventListener('click', function() {
+    console.log("wre")
+    mobileMenu.style.display = "none"
+    mobileOpen.style.display = "block"
+    mobileClose.style.display = "none"
+})
